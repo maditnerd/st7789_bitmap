c7d5618ce8520a4e09e89d96c50d2a617ac4af8b
Rémi Sarrailh <remi@madnerd.org>
2019-03-23 13:56:58 +0100

    Change to TFT_eSPI (better performance)

--- st7789_bitmap.ino
+++ st7789_bitmap.ino
@@ -1,36 +1,22 @@
-#include <SPI.h>
-#include <Adafruit_GFX.h>    // Core graphics library by Adafruit
-#include <Arduino_ST7789.h> // Hardware-specific library for ST7789 (https://github.com/ananevilya/Arduino-ST7789-Library)
-
-#include "bitmap.h" //(Bitmap generated with LCD Image Converter) https://sourceforge.net/projects/lcd-image-converter/
-
 // This example is based on this tutorial
 // https://www.instructables.com/id/Converting-Images-to-Flash-Memory-Iconsimages-for-/
 // https://github.com/STEMpedia/eviveProjects/blob/master/imageToFlashMemoryIconsForTFT/
-#define TFT_DC    D1
-#define TFT_RST   D0
-Arduino_ST7789 tft = Arduino_ST7789(TFT_DC, TFT_RST);
+
+//Don't forget to change User_Setup.h inside TFT_eSPI library !
+
+#include <TFT_eSPI.h>
+#include "bitmap.h" //(Bitmap generated with LCD Image Converter) https://sourceforge.net/projects/lcd-image-converter/
+TFT_eSPI tft = TFT_eSPI();   // Invoke library
 
 void setup(void) {
   Serial.begin(115200);
   Serial.print("ST7789 TFT Bitmap Test");
 
-  tft.init(240, 240);   // initialize a ST7789 chip, 240x240 pixels
-  Serial.println("Initialized");
+  tft.begin();     // initialize a ST7789 chip
+  tft.setSwapBytes(true); // Swap the byte order for pushImage() - corrects endianness
 
-  //uint16_t time = millis();
-  tft.fillScreen(BLACK);
-  int h = 240, w = 240, row, col, buffidx = 0;
-  for (row = 0; row < h; row++) { // For each scanline...
-    for (col = 0; col < w; col++) { // For each pixel...
-      //To read from Flash Memory, pgm_read_XXX is required.
-      //Since image is stored as uint16_t, pgm_read_word is used as it uses 16bit address
-      tft.drawPixel(col, row, pgm_read_word(mercy + buffidx));
-      buffidx++;
-      delay(0.0001); //Without this delay the wemos crash ?
-    }
-    Serial.println("Bitmap drawn");
-  }
+  tft.fillScreen(TFT_BLACK);
+  tft.pushImage(0,0,240,240,mercy);
 }
 
 void loop() {
